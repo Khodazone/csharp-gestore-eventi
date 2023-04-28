d2c6eba9a4003a88fcadd6ee895b2c72e9dc91d2
Khodazone <khodazone@gmail.com>
2023-04-28 11:14:41 +0200

    exception

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace GestoreEventi
 {
-    internal class Evento
+    public class Evento
     {
+        //ATTRIBUTI
+        private string titolo;
+        private DateTime data;
+        private int capienzaMassimaEvento;
+        private int numeroPostiPrenotati;
+
+        //GETTERS
+        public string GetTitolo()
+        {
+            return this.titolo;
+        }
+
+        public DateTime GetData()
+        {
+            return this.data;
+        }
+
+        public int GetCapienzaMassimaEvento() 
+        {
+            return this.capienzaMassimaEvento;
+        }
+
+        public int GetNumeroPostiPrenotati()
+        {
+            return this.numeroPostiPrenotati;
+        }
+
+
+        //SETTERS
+        public void SetTitolo(string newTitolo)
+        {
+            if (titolo == "")
+            {
+                throw new Exception("Titolo vuoto");
+            }
+
+            this.titolo = newTitolo;
+        }
+
+        public void SetData(DateTime data)
+        {
+            this.data = data;
+        }
+
+
+        //COSTRUTTORI
+
     }
 }
